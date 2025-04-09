6a6834543bd966214d0e10c5bebb8bc5e6f97b9a
Glitch (jolly-ruddy-attraction) <none>
2025-04-09 07:01:16 +0000

    🧠🎗️ Updated with Glitch

--- assets/two-pointers/script.js
+++ assets/two-pointers/script.js
@@ -3,7 +3,12 @@ const input = document.querySelector("input")
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  console.log(splitWords(input.value));
+  fetch('https://dawei-spring.herokuapp.com/permutation-i', {method: 'post', headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }, body: JSON.stringify({
+            "nums": splitWords(input.value)
+            })}).then(res => res.json()).then(a => console.log(a));
 })
 
 function splitWords(s) {
