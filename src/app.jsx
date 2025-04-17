836c520b0d1c9e9fd53ecb4520986deee3aea821
Glitch (mini-programs) <none>
2025-04-17 18:42:32 +0000

    🍎⛸️ Updated with Glitch

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
+import Wordle from "./components/wordle"
 
 /**
 * This code defines the react app
@@ -23,30 +24,6 @@ import Seo from './components/seo.jsx';
 // Home function that is reflected across the site
 export default function Home() {
   return (
-    <Router>
-      <Seo />
-      <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
-          <PageRouter />
-        </div>
-      </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
-      </footer>
-    </Router>
+    <Wordle/>
   );
 }
